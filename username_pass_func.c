10208fad3f86e602a48bc6da2e7d20c49b376103
aysegul yavuz <119297623+Aysegulyavuz55@users.noreply.github.com>
2022-12-01 17:27:36 +0300

    Update username_pass_func.c

--- username_pass_func.c
+++ username_pass_func.c
@@ -6,6 +6,16 @@
 #include "ctype.h" // for isdigit()
 
 void login(char username[], char password[]) {
+  if(strcmp(username,"aysegul")==0){
+	         if(strcmp(password,"yvz123")==0){
+			     printf("\n\nLogged In Successful");
+		}
+		
+}
+else{
+		printf("\n\nIncorrect username or password\n");
+		system("exit");
+}
 //   your code here  
 //   code burada
   
