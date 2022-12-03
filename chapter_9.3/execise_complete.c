c716a8b44b17c969e2aa4b8d618862b658de167e
HaticeSerin <119755031+HaticeSerin@users.noreply.github.com>
2022-12-03 23:09:18 +0300

    Hatice Serin
    I couldn't do it the way you posted. I did it this way.

--- chapter_9.3/execise_complete.c
+++ chapter_9.3/execise_complete.c
@@ -1,13 +1,33 @@
-#include "stdio.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 
-class Solution{
-//     your code here
-        public:
-        double myPow(double x, int n) {
+double UsAlma(double a, double us){
+    int s=0;
+    double r;
+    
+    if(us<0){
+        s=1;
+        us*=-1;
+    }
+    for(r=1;us>0; us--){
+        r*=a;
+    }
+    if(s){
+        r=1/r;
+    }
+    return r;
+}
 
-        }
-};
-
-//     your code here
+int main(){
+    double r;
+    int a,us;
+    printf("sayi giriniz: \n");
+	scanf("%d",&a);
+	printf("us sayisini giriniz:\n");
+	scanf("%d",&us);
+    r=UsAlma(a,us);
+    printf("Sonuc:%f\n",r);
+    return 0;
+}
