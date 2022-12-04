14361f48caf9e40e2ab6bdc69b7d913537c4079c
vokur16 <119763364+vokur16@users.noreply.github.com>
2022-12-04 04:53:31 +0300

    Update execise_complete.c

--- chapter_9.3/execise_complete.c
+++ chapter_9.3/execise_complete.c
@@ -1,13 +1,35 @@
-#include "stdio.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 
-class Solution{
-//     your code here
-        public:
-        double myPow(double x, int n) {
-
+double usalma(double a, int b){
+    double x;
+    if(b==0){
+        return 1;
+    }
+    else if (b>=1)
+    {   x=1;
+        for(int i=0;i<b;i++){
+            x*=a;
         }
-};
+        return x;
+    }
+    else if(b<0){
+        x=1;
+        for(int i=0;i<(-b);i++){
+            x*=a;
+        }
+        return 1/x;
+    }
+}
 
-//     your code here
+int main(){
+double taban, sonuc;
+int us;
+printf("Taban olacak reel sayiyi giriniz: ");
+scanf("%lf",&taban);
+printf("Us olacak tam sayiyi giriniz: ");
+scanf("%d",&us);
+sonuc= usalma(taban,us);
+printf("Sonuc: %lf",sonuc);
+}
