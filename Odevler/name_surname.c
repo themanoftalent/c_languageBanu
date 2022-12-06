b75aeaa6d6bdf21473bbb4b63c2614c076ba1d70
1Bartu_Paçal3 <118813470+BartuPacall@users.noreply.github.com>
2022-12-06 13:14:54 +0300

    Bartu α Paçal_Surname_name.c

--- Odevler/name_surname.c
+++ Odevler/name_surname.c
@@ -1,1 +1,20 @@
 // Display your name and surname on screen 
+#include <stdio.h>
+#include  "stdlib.h"
+
+int main()
+{
+  char name[10];
+  char surname[10];
+
+  printf("Please enter your name:");
+  scanf("%s",&name);
+
+  printf("Please enter your surname:");
+  scanf("%s",&surname);
+
+  printf("<KING WELCOME TO YOUR EMPEROR>\n");
+  printf("Name:%s Surname:%s",name,surname);
+
+  return 0;
+}
