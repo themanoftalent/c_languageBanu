32296e2bd3ef414d3234bb2e6985e0b4ded266d3
nil-32 <113453210+nil-32@users.noreply.github.com>
2022-12-06 02:39:15 -0800

    nil akyıldız
    @themanoftalent

--- Odevler/comapre.c
+++ Odevler/comapre.c
@@ -1,35 +1,26 @@
-// compare name surname 
+// compare name and surname and code it on screen. 
+#include"stdlib.h"
+#include"stdio.h"
+#include"string.h"
 
 
-#include "stdio.h"
-#include "string.h"
+int main(){
+  char name[] ="akif"; 
+  char surname[] ="ciftci";
+  char x[20];
+  char y[20];
+printf("name: ");
+scanf("%s",x);
+    printf("surname: ");
+    scanf("%s",y);
+ 
+  printf("%s %s\n", x, y);
 
-#define name "Akif"
-#define age 32
-#define favnum 3.14
-
-
-int main() {
-//    compare my name and age and favnum 
-    printf("Enter your name: ");
-    char name1[20];
-    scanf("%s", name1);
-    printf("Enter your age: ");
-    int age1;
-    scanf("%d", &age1);
-    printf("Enter your favnum: ");
-    float favnum1;
-    scanf("%f", &favnum1);
-if (strcmp(name, name1) == 0 && age == age1 && favnum == favnum1) {
+    if (strcmp(name, x) == 0 && strcmp(surname, y) == 0) {
         printf("You are Akif, I know who you are man, my favorite teacher");
     } else {
-         printf("You are not Akif, I don't know who you are as I am not that clever");
-        
-    }
-    return 0;
+        printf("You are not Akif, I don't know who you are as I am not that clever");
+
+        return 0;
+  }
 }
-
-
-
-
-
