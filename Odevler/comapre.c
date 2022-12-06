185df5174587a1be5ffc47db56349dadf03eb07c
aysegul yavuz <119297623+Aysegulyavuz55@users.noreply.github.com>
2022-12-06 13:48:24 +0300

    aysegul yavuz

--- Odevler/comapre.c
+++ Odevler/comapre.c
@@ -1,9 +1,8 @@
-
 #include "stdio.h"
 #include "string.h"
 
-#define name "Akif"
-#define surname "cifci"
+#define name "Aysegul"
+#define surname "Yavuz"
 
 
 int main() {
@@ -18,16 +17,16 @@ int main() {
     scanf("%d", &favnumber);
     if (strcmp(myname, name) == 0) {
         if (strcmp(mysurname, surname) == 0) {
-            if (favnumber == 7) {
-                printf("You are Akif cifci");
+            if (favnumber == 13) {
+                printf("You are Aysegul Yavuz");
             } else {
-                printf("You are not Akif cifci");
+                printf("You are not Aysegul Yavuz");
             }
         } else {
-            printf("You are not Akif cifci");
+            printf("You are not Aysegul Yavuz");
         }
     } else {
-        printf("You are not Akif cifci");
+        printf("You are not Aysegul Yavuz");
     }
     return 0;
     
