5bff1b80ed938f6a53e94e35c54c33dd64a3e5be
Berkant <119416073+MelalOnsra@users.noreply.github.com>
2022-12-06 14:19:29 +0300

    Berkant Acun
    @themanoftalent

--- Odevler/for Loopwitharray.c
+++ Odevler/for Loopwitharray.c
@@ -1,38 +1,11 @@
-// For loop with 2D array myarray[2][2]
+#include <stdio.h>
 
-// without for loop 
+int main(void)
+{
+  int idizi[2][2] = {{1,2}{3,4}};
+  int id;
 
-#include "stdio.h"
-int main(){
-    int myarray[2][2] ={
-            {1,2},
-            {3,4}
-    };
-    printf("myarray[0][0]=%d\n",myarray[0][0]);
-    printf("myarray[0][1]=%d\n",myarray[0][1]);
-    printf("myarray[1][0]=%d\n",myarray[1][0]);
-    printf("myarray[1][1]=%d\n",myarray[1][1]);
-    return 0;
-}
+  for (id=0; id<5; id++) printf("%d ", idizi[id]);
 
-############################################################
-
-// with for loop 
-
-
-#include "stdio.h"
-
-int main(){
-    int myarray[2][2]= {
-            {1,2},
-            {3,4}
-    };
-   int i,j;
-    for(i=0;i<2;i++){
-        for(j=0;j<2;j++){
-            printf("%d",myarray[i][j]);
-        }
-        printf("\n");
-    }
-    return 0;
+  return 0;
 }
