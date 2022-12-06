2b1b374085a7ac2b79c717c4b8419d395270aeab
Burak SARAN <buraksaran@tutanota.com>
2022-12-07 01:49:28 +0300

    Burak SARAN akif.c

--- Odevler/Harfler/akif.c
+++ Odevler/Harfler/akif.c
@@ -1,4 +1,36 @@
-// Make your program write "Akif" in a traianlge shape. 
-// you have the code for ABC
-// imagine, think, search and do it.
-// Best luck!
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+	int x,y,n,m;
+	char a='A',k='k',i='i',f='f';
+
+	printf("\nenter the amount of rows: ");
+	scanf("%d",&n);
+
+	printf("\n");
+	for(x=1;x<=n;x++) {
+		for(y=1;y<=2*(n-x);y++) {
+			printf(" ");
+		}
+		for(y=1;y<=x;y++) {
+			printf("%c",a);
+		}
+		printf(" ");
+		for(y=1;y<=x;y++) {
+			printf("%c",k);
+		}
+		printf(" ");
+		for(y=1;y<=x;y++) {
+			printf("%c",i);
+		}
+		printf(" ");
+		for(y=1;y<=x;y++) {
+			printf("%c",f);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
