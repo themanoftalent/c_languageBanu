9ec0c6d600d5e954359255e0b4d5b205754c14a7
Kalyoncuguller <118815433+Kalyoncuguller@users.noreply.github.com>
2022-12-06 23:23:59 +0300

    Kalyoncugüller
    akif output kodu

--- Odevler/Harfler/akif.c
+++ Odevler/Harfler/akif.c
@@ -1,4 +1,30 @@
-// Make your program write "Akif" in a traianlge shape. 
-// you have the code for ABC
-// imagine, think, search and do it.
-// Best luck!
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int i,j,k;
+for (i = 1; i<=5; i++){
+    for(k = 0; k <5-i; k++){
+        printf(" ");
+    }
+    for(j = 0; j<i; j++){
+        printf("A");
+    } 
+    printf(" ");
+    for(int c = 0; c<i; c++){
+        printf("k");
+    }
+    printf(" ");
+    for(int c = 0; c<i; c++){
+        printf("i");
+    }    
+    printf(" ");
+    for(int c = 0; c<i; c++){
+        printf("f");
+    }
+
+    printf("\n");
+}
+    
+}
