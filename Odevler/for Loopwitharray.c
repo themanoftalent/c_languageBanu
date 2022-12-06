45c4f09781d8edfbdb506f5bb95f6df90cf179a5
1Bartu_Paçal3 <118813470+BartuPacall@users.noreply.github.com>
2022-12-06 14:23:32 +0300

    Bartu Paçal
    R.E.N

--- Odevler/for Loopwitharray.c
+++ Odevler/for Loopwitharray.c
@@ -4,35 +4,39 @@
 
 #include "stdio.h"
 int main(){
-    int myarray[2][2] ={
+    int numbers[2][2] ={
             {1,2},
             {3,4}
     };
-    printf("myarray[0][0]=%d\n",myarray[0][0]);
-    printf("myarray[0][1]=%d\n",myarray[0][1]);
-    printf("myarray[1][0]=%d\n",myarray[1][0]);
-    printf("myarray[1][1]=%d\n",myarray[1][1]);
+    printf("numbers[0][0]=%d\n",numbers[0][0]);
+    printf("numbers[0][1]=%d\n",numbers[0][1]);
+    printf("numbers[1][0]=%d\n",numbers[1][0]);
+    printf("numbers[1][1]=%d\n",numbers[1][1]);
     return 0;
 }
 
-############################################################
 
 // with for loop 
 
 
-#include "stdio.h"
+#include <stdio.h>
 
-int main(){
-    int myarray[2][2]= {
-            {1,2},
-            {3,4}
-    };
-   int i,j;
-    for(i=0;i<2;i++){
-        for(j=0;j<2;j++){
-            printf("%d",myarray[i][j]);
-        }
-        printf("\n");
+int main() { // this is the main function that will be called when the program is run
+    
+     int j;
+     int i;
+
+    int numbers[2][2]= {{1,2}, {3,4}}; //here we have 2 rows and 2 columns in the array
+ 
+    for(i=0;i<2;i++)
+    {
+      for(j=0;j<2;j++)
+      {
+            printf("The first element is %d\n", numbers[i][j]); 
+           
+      }
     }
+
+    
     return 0;
 }
