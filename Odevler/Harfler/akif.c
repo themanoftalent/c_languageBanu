2e624ee16c88ad8d1c7d38b1def0f7d753126efd
Burak SARAN <buraksaran@tutanota.com>
2022-12-07 02:45:43 +0300

    Burak SARAN akif.c correction

--- Odevler/Harfler/akif.c
+++ Odevler/Harfler/akif.c
@@ -7,27 +7,33 @@ int main() {
 	printf("\nenter the amount of rows: ");
 	scanf("%d",&n);
 
-	printf("\n");
-	for(x=1;x<=n;x++) {
-		for(y=1;y<=2*(n-x);y++) {
-			printf(" ");
-		}
-		for(y=1;y<=x;y++) {
-			printf("%c",65);
-		}
-		printf(" ");
-		for(y=1;y<=x;y++) {
-			printf("%c",107);
-		}
-		printf(" ");
-		for(y=1;y<=x;y++) {
-			printf("%c",105);
-		}
-		printf(" ");
-		for(y=1;y<=x;y++) {
-			printf("%c",102);
-		}
+	if(n==0) {
+		printf("\nthere is nothing to print.\n");
+	}
+	else {
+
 		printf("\n");
+		for(x=1;x<=n;x++) {
+			for(y=1;y<=2*(n-x);y++) {
+				printf(" ");
+			}
+			for(y=1;y<=x;y++) {
+				printf("%c",65);
+			}
+			printf(" ");
+			for(y=1;y<=x;y++) {
+				printf("%c",107);
+			}
+			printf(" ");
+			for(y=1;y<=x;y++) {
+				printf("%c",105);
+			}
+			printf(" ");
+			for(y=1;y<=x;y++) {
+				printf("%c",102);
+			}
+			printf("\n");
+		}
 	}
 
 	return 0;
