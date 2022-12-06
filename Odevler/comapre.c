a9bcffae473d38f776c616fad7b037c52bf6b53a
ilayda aytaş <119287638+8435726@users.noreply.github.com>
2022-12-06 13:48:29 +0300

    Update comapre.c
    @thethemanoftalent

--- Odevler/comapre.c
+++ Odevler/comapre.c
@@ -2,8 +2,8 @@
 #include "stdio.h"
 #include "string.h"
 
-#define name "Akif"
-#define surname "cifci"
+#define name "ilayda"
+#define surname "Aytaş"
 
 
 int main() {
@@ -11,23 +11,23 @@ int main() {
     char mysurname[10];
     int favnumber;
     printf("Enter your name:");
-    scanf("%s", myname);
-    printf("Enter your surname:");
-    scanf("%s", mysurname);
-    printf("Enter your favourite number:");
+    scanf("%s",& myname);
+    printf("Enter your surname:\n");
+    scanf("%s",& mysurname);
+    printf("Enter your favourite number:\n");
     scanf("%d", &favnumber);
     if (strcmp(myname, name) == 0) {
         if (strcmp(mysurname, surname) == 0) {
             if (favnumber == 7) {
-                printf("You are Akif cifci");
+                printf("You are ilayda Aytaş");
             } else {
-                printf("You are not Akif cifci");
+                printf("You are not ilayda Aytaş");
             }
         } else {
-            printf("You are not Akif cifci");
+            printf("You are not ilayda Aytaş");
         }
     } else {
-        printf("You are not Akif cifci");
+        printf("You are not ilayda Aytaş");
     }
     return 0;
     
