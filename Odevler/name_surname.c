fcefa41be5c3dce068c7cb2e3610ef936b98fbb3
Akif Ali Akbalık <111509037+akbaliakif@users.noreply.github.com>
2022-12-06 13:19:04 +0300

    Update name_surname.c
    @themanoftalent 
    
    akif ali akbalik
    
    #include <stdio.h>
    #include <stdlib.h>
    
    int main()
    {
    
        char name[10] = "akif";
        char surname[20] = "akbalik";
    
        printf("your name is %s ",name);
        printf("and your surname is %s",surname);
    
    
    
        return 0;
    }

--- Odevler/name_surname.c
+++ Odevler/name_surname.c
@@ -1,1 +1,16 @@
-// Display your name and surname on screen 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    char name[10] = "akif";
+    char surname[20] = "akbalik";
+
+    printf("your name is %s ",name);
+    printf("and your surname is %s",surname);
+
+
+
+    return 0;
+}
