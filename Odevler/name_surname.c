aa26fbf172f403c6d6dae0f45bc29121a87d1535
Kalyoncuguller <118815433+Kalyoncuguller@users.noreply.github.com>
2022-12-06 23:03:51 +0300

    Kalyoncugüller
    username password

--- Odevler/name_surname.c
+++ Odevler/name_surname.c
@@ -1,1 +1,27 @@
-// Display your name and surname on screen 
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "ctype.h" 
+
+typedef struct login {
+    char username[20];
+    char password[20];   
+} login;
+
+void main() {
+    login *ptr;
+    int n, i;
+    printf("Enter the number of employees: ");
+    scanf("%d", &n);
+    ptr = (login *) malloc(n * sizeof(login));
+    for (i = 0; i < n; i++) {
+        printf("Enter the username of employee %d: ", i + 1);
+        scanf("%s", ptr[i].username);
+        printf("Enter the password of employee %d: ", i + 1);
+        scanf("%s", ptr[i].password);
+    }
+    printf(" Username\t\tPassword of employees \t  ");
+    for (i = 0; i < n; i++) {
+        printf(" %s\t\t%s \t  ", ptr[i].username, ptr[i].password);
+    }
+}
