300f0f04afe9e6ec8be2fd32225067798d291977
1Bartu_Paçal3 <118813470+BartuPacall@users.noreply.github.com>
2022-12-08 00:43:42 +0300

    Bartu α Paçal_find_error_fix_it
    :)

--- Odevler/hata_bul.c
+++ Odevler/hata_bul.c
@@ -1,16 +1,17 @@
-#include <stdion.h>
+#include <stdio.h> //WE WROTE FALSE LİBRARY NAME
 int main()
 {
-    char name[0][10], i
-    printf("Enter the 3 name :\n");
+    char name[10][10]; //we use ; instead , we got an error because we used and we use 
+    int i; // we forgot to declare i
+    printf("Enter three name:\n"); //we brought printf here
     for (i = 0; i < 3; i++)
     {
-        printf("Enter the sring :");
-        scanf("%s", &name[i]);
+    scanf("%s", &name[i]); // delete other printf because it was making the code too crowded :)
     }
     for (i = 0; i < 3; i++)
     {
         printf("%s\n", &name[i]);
     }
-    return 10;
+    return 0; // we wrote 0 instead 10
 }
+//R.E.N B.P
