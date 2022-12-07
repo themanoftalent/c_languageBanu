91cf12356e491bf8f1972b6e1121e32bdc0b0311
nil-32 <113453210+nil-32@users.noreply.github.com>
2022-12-07 00:07:13 -0800

    nil akyıldız
    upon entering name, prints it as a right-aligned pyramid

--- Odevler/Harfler/akif.c
+++ Odevler/Harfler/akif.c
@@ -2,3 +2,27 @@
 // you have the code for ABC
 // imagine, think, search and do it.
 // Best luck!
+#include <stdio.h>
+
+int main(){
+
+      int k,i,row;
+      char name[25];
+      printf("enter your name\n");
+      scanf("%s",&name);
+      int x = printf(name);
+      printf("\n\n");
+	
+	for(row = 1; row <= x; row++)                      //OK
+   {
+        for(i = x - row - 1; i >= 0; i--)            //OK
+        {
+        printf(" ");                                 //OK
+        }
+        for ( k = 0; k < row ; k++)                  //OK
+        {
+        printf("%c",name[k]);                        //OK
+        }
+    printf("\n");
+    }
+}
