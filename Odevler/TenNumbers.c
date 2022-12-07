bce1acfe915382c0bbbb19775c21b44f118e3509
1Bartu_Paçal3 <118813470+BartuPacall@users.noreply.github.com>
2022-12-08 01:00:29 +0300

    Bartu α Paçal_Ten_Numbers_UPDATE
    :)

--- Odevler/TenNumbers.c
+++ Odevler/TenNumbers.c
@@ -1,9 +1,42 @@
 //AKIF
 //C program to input 10 numbers from user and display 10 numbers using array.
 
-#include <stdio.h>
+
+#include "stdio.h"
 int main()
 {
-//   YOUR CODE HERE
-  
+    int number[10];
+    printf("Enter ten number:\n");
+    for(int i=0;i<10;i++)
+    {
+        scanf("%d",&number[i]);
+    }
+    for(int i=0;i<10;i++)
+    {
+        printf("%d",number[i]);
+    }
+    return 0;
 }
+-------------------------------------------------------------------------------------------------
+  
+#include "stdio.h"
+int main()
+{
+    int number[10];
+    printf("Enter ten number:\n");
+    for(int i=0;i<10;i++)
+    {
+        scanf("%d",&number[i]);
+   
+    }
+    int i=0;
+       while(i<10)
+    {
+        i++;
+        printf("%d",number[i-1]);
+        
+    }  
+    return 0;
+}
+
+
