eef155a07d7f7201521c0571f91e29c1aaabc902
nil-32 <113453210+nil-32@users.noreply.github.com>
2022-12-07 00:35:03 -0800

    akif.c  nil akyıldız
    @themanoftalent

--- Odevler/Harfler/akif.c
+++ Odevler/Harfler/akif.c
@@ -2,3 +2,33 @@
 // you have the code for ABC
 // imagine, think, search and do it.
 // Best luck!
+#include <stdio.h>
+
+int main(){
+
+int k,i,row,b;
+char name[25];
+printf("enter your name\n");
+scanf("%s",&name);
+int x = printf(name);
+printf("\n\n");
+
+  for(row = 1; row <= x; row++)
+  {
+        for(i = x - row - 1; i >= 0; i--)
+         {
+         printf(" ");
+         }
+    
+        for(b=0; b<x; b++)//ordered
+        {
+            for ( k = 0; k < row ; k++)//how many times of that letter
+            {
+            printf("%c",name[b]);
+            }
+        printf(" ");
+        }
+    
+  printf("\n");
+  }
+}
