3b76fb4c2e97d666c6cc58324146059f19bcb5e2
Vahdettin Okur <119763364+vokur16@users.noreply.github.com>
2022-12-08 15:02:03 +0300

    Vahdettin Okur;ifNameThisThat

--- Odevler/2.ifNameThisThat.c
+++ Odevler/2.ifNameThisThat.c
@@ -1,1 +1,30 @@
-// compare name and surname and code it on screen. 
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    char name[] = "Vahdettin";
+    char surname[] ="Okur";
+
+    char namei[20];
+    char surnamei[20];
+    printf("Enter your name: \n");
+    scanf("%s",&namei);
+    printf("Enter your surname: \n");
+    scanf("%s",&surnamei);
+
+
+    int ns = strcmp(namei,name);
+    int ss = strcmp(surnamei,surname);
+
+    if ((!ns)&(!ss)){
+        printf("\nWelcome %s %s",name,surname);
+    }
+    else if((!ns)^(!ss)){
+        printf("\nOne of them is incorrect");
+    }
+    else{
+        printf("\nAll of them are incorrect");
+    }
+
+}
