479851f4dd961334a6bf8b61854ef56c5a14a7c4
aysegul yavuz <119297623+Aysegulyavuz55@users.noreply.github.com>
2022-12-08 09:11:18 +0300

    Update hata_bul.c

--- Odevler/hata_bul.c
+++ Odevler/hata_bul.c
@@ -1,16 +1,18 @@
-#include <stdion.h>
+
+#include <stdio.h>
 int main()
 {
-    char name[0][10], i
+    char name[10][10], i;
     printf("Enter the 3 name :\n");
+
+    
     for (i = 0; i < 3; i++)
     {
-        printf("Enter the sring :");
         scanf("%s", &name[i]);
     }
     for (i = 0; i < 3; i++)
     {
         printf("%s\n", &name[i]);
     }
-    return 10;
+    return 0;
 }
