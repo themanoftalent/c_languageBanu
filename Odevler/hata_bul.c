5fd9036da0308662c170a9ed69ff38f40293d8fe
Kalyoncuguller <118815433+Kalyoncuguller@users.noreply.github.com>
2022-12-08 11:14:46 +0300

    Kalyoncugüller
    Hata düzeltildi.

--- Odevler/hata_bul.c
+++ Odevler/hata_bul.c
@@ -1,11 +1,12 @@
-#include <stdion.h>
+#include <stdio.h>
 int main()
 {
-    char name[0][10], i
+    char name[3][10];
+    int i;
     printf("Enter the 3 name :\n");
     for (i = 0; i < 3; i++)
     {
-        printf("Enter the sring :");
+        printf("%d.Enter the string :",i+1);
         scanf("%s", &name[i]);
     }
     for (i = 0; i < 3; i++)
