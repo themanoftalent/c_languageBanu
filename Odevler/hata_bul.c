fe5e40ab69f83820de5f9c7d58ef4f4733e7be6c
halil-ibrhm-ky <119860358+halil-ibrhm-ky@users.noreply.github.com>
2022-12-08 19:09:49 +0300

    Halil İbrahim Kaya
    @themanoftalent

--- Odevler/hata_bul.c
+++ Odevler/hata_bul.c
@@ -1,16 +1,22 @@
-#include <stdion.h>
+#include<stdlib.h>
+#include<stdlib.h>
+#include<string.h>
+
+
 int main()
 {
-    char name[0][10], i
+    char name[3][10];
+
     printf("Enter the 3 name :\n");
-    for (i = 0; i < 3; i++)
-    {
-        printf("Enter the sring :");
-        scanf("%s", &name[i]);
-    }
-    for (i = 0; i < 3; i++)
-    {
-        printf("%s\n", &name[i]);
-    }
-    return 10;
+int i;
+for( i=0; i<3; i++)
+{
+    scanf("%s",&name[i]);
+}
+
+for(i=0; i<3; i++)
+{
+    printf("%s\t",name[i]);
+}
+return 0;
 }
