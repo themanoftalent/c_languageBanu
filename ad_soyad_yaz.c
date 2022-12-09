a81f8f49eb0b9c860c6e91655ff41ef06e1c4052
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-12-09 20:17:21 +0300

    Create ad_soyad_yaz.c

---
+++ ad_soyad_yaz.c
@@ -0,0 +1,21 @@
+// Merhaba, 
+// Adini soyadini yasini yaz ekrana bastir.
+// sonra kayit etmeden once @themanoftalent tag yap. 
+// Sevgiler
+// Iyi hafta sonrlari
+
+#include <stdio.h>
+
+int main() {
+
+
+  //CODE BUARAYA YAZILACAK 
+//   String icin char kullan ve &kullanma, & sadece sayilar icin kullan. 
+
+
+
+
+
+
+
+}
