d6b6282d701cdd7f3f6fb8922efb52c709f8aa68
MOHAMED ELIDRYSY <75158922+Medo3215@users.noreply.github.com>
2022-12-12 11:36:03 +0100

    Update float_int.c

--- float_int.c
+++ float_int.c
@@ -1,4 +1,25 @@
 
+#include <stdio.h>
+#include <string.h>
 
-// Akif was here 😇
-//Gosh!
+int main()
+{
+    int i,j,k;
+    char name[50];
+
+    printf("enter your name : ");
+    scanf("%s", &name);
+    k = strlen(name);
+  
+for (i = 0; i<k; i++){
+    for(j = 50; j > i; j--){
+        printf(" ");
+    }
+    for(j = 0; j<=i; j++){
+        printf("%c", name[j]);
+        printf(" ");
+    }
+    printf("\n");
+}
+}
+
